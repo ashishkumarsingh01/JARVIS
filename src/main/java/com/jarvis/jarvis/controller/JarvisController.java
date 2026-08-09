d3511d670792564dfc621121e42ad6f409765df7
Ashish Kumar Singh <ashishkumarsingh8866@gmail.com>
2026-08-09 12:53:28 +0530

    Enable CORS for React frontend

--- src/main/java/com/jarvis/jarvis/controller/JarvisController.java
+++ src/main/java/com/jarvis/jarvis/controller/JarvisController.java
@@ -5,10 +5,12 @@ import com.jarvis.jarvis.tools.PhoneTools;
 import com.jarvis.jarvis.tools.WebTools;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class JarvisController {
 
